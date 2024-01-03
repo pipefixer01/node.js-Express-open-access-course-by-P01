76c139debea6f2473272822ceeb87b9a662d46d6
pipefixer01 <126138336+pipefixer01@users.noreply.github.com>
2024-01-03 12:28:51 +0700

    new onepagelove

--- public/CHANGELOG.md
+++ public/CHANGELOG.md
@@ -1,54 +1,49 @@
 # CHANGELOG.md
 
-## 1.1.1 (Oct 5, 2018)
+## 1.1.0 (Oct 5, 2018)
 
 ##### Update license and PDF
-* /Agnes.pdf
+* /Ava.pdf
 * /package-sample.json
 * /LICENSE
 
-## 1.1.0 (Sep 25, 2018)
+## 1.0.9 (Sep 25, 2018)
 
 ##### Fix minor typographic issue and small buttons fix
 * /src/scss/base/_typography.scss
 * /src/scss/components/_buttons.scss
 
-## 1.0.9 (Sep 18, 2018)
+## 1.0.8 (Sep 18, 2018)
 
 ##### Fix forms and buttons height issue on Safari
 * /src/scss/components/_buttons.scss
+* /src/scss/components/_forms.scss
 
-## 1.0.8 (Sep 17, 2018)
+## 1.0.7 (Sep 17, 2018)
 
 ##### Update template name
 * /README.md
 
-## 1.0.7 (Sep 15, 2018)
+## 1.0.6 (Sep 15, 2018)
 
 ##### Minor CSS fix
 * /src/scss/layout/_features.scss
 
-## 1.0.6 (Sep 13, 2018)
+## 1.0.5 (Sep 13, 2018)
 
 ##### Minor CSS fix
 * /src/scss/base/_base.scss
-* /src/scss/layout/_hero.scss
 
-## 1.0.5 (Sep 11, 2018)
+## 1.0.4 (Sep 11, 2018)
 
 ##### Update license
 * /package-sample.json
 * /LICENSE.rtf
 
-## 1.0.4 (Sep 11, 2018)
+## 1.0.3 (Sep 11, 2018)
 
 ##### Keep animating elements visible when animations are disabled
 * /src/scss/base/_helpers.scss
-
-## 1.0.3 (Sep 10, 2018)
-
-##### Fix issue with not clickable button
-* /src/scss/layout/_pricing.scss
 
 ## 1.0.2 (Sep 10, 2018)
 
--- public/src/js/main.js
+++ public/src/js/main.js
@@ -9,48 +9,28 @@
     /* global ScrollReveal */
     const sr = window.sr = ScrollReveal()
 
-    sr.reveal('.hero-title, .hero-paragraph, .hero-cta', {
+    sr.reveal('.hero-title, .hero-paragraph, .newsletter-header, .newsletter-form', {
       duration: 1000,
       distance: '40px',
       easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
-      origin: 'left',
+      origin: 'bottom',
       interval: 150
     })
 
-    sr.reveal('.hero-illustration', {
+    sr.reveal('.bubble-3, .bubble-4, .hero-browser-inner, .bubble-1, .bubble-2', {
       duration: 1000,
-      distance: '40px',
+      scale: 0.95,
       easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
-      origin: 'right',
       interval: 150
     })
 
     sr.reveal('.feature', {
-      duration: 1000,
+      duration: 600,
       distance: '40px',
       easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
       interval: 100,
       origin: 'bottom',
-      scale: 0.9,
       viewFactor: 0.5
-    })
-
-    const pricingTables = document.querySelectorAll('.pricing-table')
-
-    pricingTables.forEach(pricingTable => {
-      const pricingTableHeader = [].slice.call(pricingTable.querySelectorAll('.pricing-table-header'))
-      const pricingTableList = [].slice.call(pricingTable.querySelectorAll('.pricing-table-features li'))
-      const pricingTableCta = [].slice.call(pricingTable.querySelectorAll('.pricing-table-cta'))
-      const elements = pricingTableHeader.concat(pricingTableList).concat(pricingTableCta)
-
-      sr.reveal(elements, {
-        duration: 600,
-        distance: '20px',
-        easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
-        interval: 100,
-        origin: 'bottom',
-        viewFactor: 0.5
-      })
     })
   }
 }())
