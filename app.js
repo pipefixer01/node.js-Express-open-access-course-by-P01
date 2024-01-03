3b381d255c05e6c15f9015c7c3066c87ace282d5
pipefixer01 <126138336+pipefixer01@users.noreply.github.com>
2024-01-03 15:39:02 +0700

    reinstall nodemon with template
    it crash because i uninstall nodemon in app.js it consequences to index.html, i fix it by revert my template and install nodemon then it alright.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")));
