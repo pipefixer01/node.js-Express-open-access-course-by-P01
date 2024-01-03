c98913140996bbe60412715308dcb5ec5ceaf9d1
pipefixer01 <126138336+pipefixer01@users.noreply.github.com>
2024-01-03 12:08:24 +0700

    adjust app.js and add index.html

--- app.js
+++ app.js
@@ -2,18 +2,20 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/public/")));
 
-app.get("/", (req,res) =>{
+app.get("/", (req, res) => {
 
     res.send('Hello borntoDev Co, Ltd.');
 
 })
 
-app.listen(port, ()=>{
-    debug("Listening on port "+ chalk.green(port));
+app.listen(port, () => {
+    debug("Listening on port " + chalk.green(port));
 })
