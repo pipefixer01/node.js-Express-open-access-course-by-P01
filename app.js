f4ffabb1fd15cad822a718bec144947e4031bff6
pipefixer01 <126138336+pipefixer01@users.noreply.github.com>
2024-01-05 13:05:49 +0700

    adjust product second
    add router

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const productRouter = express.Router();
 
 const app = express();
 const port = process.env.PORT;
@@ -10,14 +11,22 @@ const port = process.env.PORT;
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")));
 
-app.set("views","./src/views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-app.get("/products");
+productRouter.route("/").get((req, res) => {
+    res.send("Hello World !! I'm Product");
+});
+
+productRouter.route("/").get((req, res) => {
+    res.send("Hello World !! I'm Product 1");
+});
+
+app.use("/products", productRouter);
 
 app.get("/", (req, res) => {
 
-    res.render('index',{username: 'Jhon 009', customers:["Jo","Lazia","Rana"]});
+    res.render('index', { username: 'Jhon 009', customers: ["Jo", "Lazia", "Rana"] });
 
 })
 
